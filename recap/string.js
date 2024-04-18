fac0da9501171527cb9eef04df69d5b38fe0f9d5
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-18 11:01:24 +0530

    string Methods

--- recap/string.js
+++ recap/string.js
@@ -19,29 +19,79 @@
 // always length start : 1 number
 // always index start : 0 number 
 // that means  last index = length-1
-
-
-var str1 = "Royal tecnosoft p ltd.";
+///////////////////////////////////////////////////////////////
+var str1 = "Royal  tecnosoft p ltd.";
 console.log(str1);
-
-
 // str1.charAt
+                //index
+// console.log(str1.charAt(21));
+////////////////////////////////////////////
 // str1.charCodeAt
+
+// console.log(str1.charCodeAt(0));
+
+// if(str1.charCodeAt(0)>=65 && str1.charCodeAt(0)<=90)
+// {
+//     console.log("Valid");
+// }
+
+// if(str1.charAt(0)>='A' && str1.charAt(0)<='Z')
+// {
+//     console.log("Valid");
+// }
+///////////////////////////////////////
 // str1.codePointAt
+// console.log(str1.codePointAt(30));
+
+///////////////////////////////////////
 // str1.concat
+// var str2 = "Zafar";
+// console.log(str1.concat(str2));
+// console.log(str1+str2);
+
+//////////////////////////////////////
 // str1.endsWith
+// .   ?
+// if(str1.endsWith('  '))
+// {
+//     console.log("done");
+// }
+////////////////////////////////////
 // str1.includes
+
+// console.log(str1.includes("p l"));
+/////////////////////////////////////
 // str1.indexOf
+console.log(str1.indexOf('o'));
+////////////////////////////////////
 // str1.lastIndexOf
+console.log(str1.lastIndexOf('o'));
+//////////////////////////////////
 // str1.length
+///////////////////////////////////
 // str1.replace
-// str1.slice
+// console.log(str1.replace('o','O'));
+// console.log(str1.replaceAll('o','O'));
+//////////////////////////////////
 // str1.split
+// var ans = str1.split(' ');
+// console.log(ans.length-1);
+/////////////////////////////////////
 // str1.startsWith
-// str1.substring
+// console.log(str1.startsWith('R'));
+///////////////////////////////
+
 // str1.toLowerCase
+console.log(str1.toLowerCase());
+///////////////////////////////
 // str1.toUpperCase
-// str1.trim
+// console.log(str1.toUpperCase());
+///////////////////////////////////
+
+ // str1.trim
 // str1.trimStart
 // str1.trimEnd
 // str1.valueOf
+// str1.slice
+// str1.substring
+// str1.substr 
