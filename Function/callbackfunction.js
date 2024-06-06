6b256de35f539d5e37e6f4f1654ef16f497da3fc
Shabhaizafar <zafarShabhai03@gmail.com>
2024-06-06 10:54:45 +0530

    Callback_Function

---
+++ Function/callbackfunction.js
@@ -0,0 +1,34 @@
+// Simple Callback Function 
+// function Addition(a,b){
+//     console.log("Addition of A and B : ",a+b);
+// }
+// function Sub(a,b){
+//     console.log("Subtraction of A and B : ",a-b);
+// }
+
+// function All(){
+//     var n1 = +prompt("Enter the Value of N1 : ");
+//     var n2 = +prompt("Enter the Value of N2 : ");
+//     var choice = +prompt("1) for Addition\n2) for Subtraction\n\nEnter Your Choice : ");
+
+//     switch(choice)
+//     {
+//         case 1 : Addition(n1,n2);
+//         break;
+//         case 2 : Sub(n1,n2);
+//         break;
+//         default : console.log("Please Enter Valid Choice !!!");
+//     }
+// };
+// All();
+
+///////////////////////////////////
+// function fu2(){
+//     console.log("Outer Function");
+// }
+
+// function Hello(callback){
+//     console.log("Hello Everyone !!");
+//     callback();
+// }
+// Hello(fu2);
